debb3ce8a09d8fe941be5fbf4f8bf58a57ec3245
wilkeska <142601106+wilkeska@users.noreply.github.com>
2023-10-12 17:29:30 -0400

    improved vtk to csv functionality

---
+++ vtk2csv.py
@@ -0,0 +1,30 @@
+import subprocess
+import os
+
+pvpython_path = "C:\\Program Files\\ParaView 5.11.2\\bin\\pvpython.exe"
+
+run_folder = '14.275.16_27.5kW_CF4_port4_16stream'
+main_dir = os.path.join(os.getenv('USERPROFILE'), 'Desktop', 'PFAS_Modeling', 'cfs_runs')
+destruct_file = os.path.join(main_dir, run_folder, 'excel\\destruct.csv')
+destruct_file = destruct_file.replace('\\','/')
+script_dir = os.path.join(main_dir, run_folder, 'excel\\convert_script.py')
+
+vtk_path  = os.path.join(main_dir, run_folder, 'streamline_pp.vtk')
+vtk_path = vtk_path.replace('\\','/')
+script_content = "from paraview.simple import *\nreader = OpenDataFile('" + vtk_path + "')\nSaveData('"+ destruct_file + "', proxy=reader)"
+
+
+def run_pvpython():
+    subprocess.run([pvpython_path, script_dir], shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text = 'TRUE')
+
+if not os.path.exists(destruct_file): #check if destruct.csv exists
+    print('No destruct.csv, creating now') #if not, create it
+    if not os.path.exists(script_dir): #check if convert_script.py exists
+        print('No convert_script.py, creating now') #if not, create it
+        with open(script_dir, "w") as file: #write script to convert vtk to csv
+                file.write(script_content)
+    else: #if convert_script.py already exists, run pvpython
+        print('convert_script.py already exists, skipping creation')
+    run_pvpython()
+else: #if destruct.csv already exists, skip creation
+    print('Destruct.csv already exists, skipping creation')
